7ed6970c5a08b3b5e0f98db244beeffeb02f5d7f
N-i-ke <nike.forgithub@gmail.com>
2024-04-23 01:15:53 +0900

    useStateの勉強

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
+import { useState } from 'react';
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
-  const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px",
-  };
-  const contentStyleB = {
-    color: "blue",
-    fontSize: "18px",
-  };
+  //stateで状態管理　[num, setNum] = useState()　初期値,更新変数　
+ //関数の一番上でしか呼べない
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum(num+1)
+  }  
   return (
     <div>
       <h1 style={{ color: "red" }}>こんにちは</h1>
@@ -18,7 +16,8 @@ export const App = () => {
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="green" message="大丈夫です" />
       <ColorfulMessage color="blue">元気です。</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+          <p>{num}</p>  
     </div>
   );
 };
